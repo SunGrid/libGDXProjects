3d48a277c58aa28d595a6c46b33995d8404ce55b
Michael <mlonnecker@gmail.com>
2019-07-25 15:20:44 -0600

    InputMultiplexer(firstProcessor, secondProcessor)

---
+++ core/src/com/vivospice/sample/MultiplexerSample.kt
@@ -0,0 +1,54 @@
+package com.vivospice.sample
+
+import com.badlogic.gdx.*
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.vivospice.sample.utils.logger
+
+class MultiplexerSample : ApplicationAdapter() {
+
+    companion object {
+        @JvmStatic
+        private val log = logger<MultiplexerSample>()
+    }
+
+    override fun create() {
+        Gdx.app.logLevel = Application.LOG_DEBUG
+
+        //val multiplexer = InputMultiplexer()
+
+        // anonymous inner class
+        val firstProcessor = object : InputAdapter() {
+            override fun keyDown(keycode: Int): Boolean {
+                log.debug("first keyDown keycode= $keycode")
+                return true
+            }
+
+            override fun keyUp(keycode: Int): Boolean {
+                log.debug("first keyUp keycode= $keycode")
+                return false
+            }
+        }
+
+        val secondProcessor = object : InputAdapter() {
+            override fun keyDown(keycode: Int): Boolean {
+                log.debug("second keyDown keycode= $keycode")
+                return true
+            }
+
+            override fun keyUp(keycode: Int): Boolean {
+                log.debug("second keyUp keycode= $keycode")
+                return false
+            }
+        }
+
+        //multiplexer.addProcessor(firstProcessor)
+        //multiplexer.addProcessor(secondProcessor)
+
+        //Gdx.input.inputProcessor = multiplexer
+        Gdx.input.inputProcessor = InputMultiplexer(firstProcessor, secondProcessor)
+
+    }
+
+}
